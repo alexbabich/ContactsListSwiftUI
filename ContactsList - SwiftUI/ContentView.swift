2b57107b16155165414ca019c27f1e085d1b5d18
alex-babich <ababich.dev@gmail.com>
2020-05-02 22:04:34 +0300

    1) init commit; 2) finished first part of contact list app;

--- ContactsList - SwiftUI/ContentView.swift
+++ ContactsList - SwiftUI/ContentView.swift
@@ -7,15 +7,126 @@
 //
 
 import SwiftUI
+import LBTATools
+
+enum SectionType {
+    case ceo, peasents
+}
+
+struct Contact: Hashable {
+    let name: String
+}
+
+class ContactViewModel: ObservableObject {
+    @Published var name = ""
+}
+
+struct ContactRowView: View {
+    
+    @ObservedObject var viewModel: ContactViewModel
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "person.fill")
+            Text(viewModel.name)
+            Spacer()
+            Image(systemName: "star")
+        }.padding(20)
+    }
+}
+
+class ContactCell: UITableViewCell {
+    
+    let viewModel = ContactViewModel()
+    
+    lazy var row = ContactRowView(viewModel: viewModel)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let hostingController = UIHostingController(rootView: row)
+        addSubview(hostingController.view)
+        hostingController.view.fillSuperview()
+        
+        viewModel.name = ""
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+class DiffableTableViewController: UITableViewController {
+    
+//    UITableView
+    lazy var source: UITableViewDiffableDataSource<SectionType, Contact> = .init(tableView: self.tableView) {
+        (_, indexPath, contact) -> UITableViewCell? in
+        
+        let cell = ContactCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = contact.name
+        cell.viewModel.name = contact.name
+        return cell
+    }
+    
+    private func setupSource() {
+        var snapshot = source.snapshot()
+        snapshot.appendSections([.ceo, .peasents])
+        snapshot.appendItems([
+            .init(name: "Elon Mask"),
+            .init(name: "Tim Cook"),
+            .init(name: "whatever")
+        ], toSection: .ceo)
+        
+        snapshot.appendItems([
+            .init(name: "Bill Gates"),
+            .init(name: "Bill Clinton")
+        ], toSection: .peasents)
+    
+        source.apply(snapshot)
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        
+        label.text = section  == 0 ? "CEO" : "Peasants"
+        return label
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        setupSource()
+    }
+}
+
+struct DiffableContainer: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        UINavigationController(rootViewController: DiffableTableViewController(style: .insetGrouped))
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+//
+    }
+    
+    typealias UIViewControllerType = UIViewController
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("2342")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        DiffableContainer()
     }
 }
---
+++ Pods/Target Support Files/Pods-ContactsList - SwiftUI/Pods-ContactsList - SwiftUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ContactsList___SwiftUI : NSObject
+@end
+@implementation PodsDummy_Pods_ContactsList___SwiftUI
+@end
